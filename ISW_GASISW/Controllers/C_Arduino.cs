e62c30e10bf70911dfa4ec2876d42c4264a12990
Baltazar López <baltazarh_lopez@hotmail.com>
2015-11-14 09:56:41 -0600

    Integrando Arduino

---
+++ ISW_GASISW/Controllers/C_Arduino.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Importar librerias
+using System.IO.Ports;
+
+namespace ISW_GASISW.Controllers
+{
+    
+    public class C_Arduino
+    {
+        //Puerto Serial Arduino-Server
+        SerialPort arduino = new SerialPort();
+
+        #region Establecer parametros de Conexion Arduino-servidor
+        public void inicializar()
+        {
+            arduino.PortName = "COM4";
+            arduino.BaudRate = 9600;
+        }
+        #endregion
+
+        #region Caso prueba 1
+        public void Test()
+        {
+            string ValorPrueba = "1R7";
+            this.inicializar();
+            arduino.Open();
+            arduino.Write(ValorPrueba);
+            arduino.Close();
+        }
+        #endregion
+    }
+}
---
+++ ISW_GASISW/Controllers/TestBombaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISW_GASISW.Controllers
+{
+    public class TestBombaController : Controller
+    {
+        //Llamando a la clase Arduino
+        C_Arduino CARD = new C_Arduino();
+
+        //
+        // GET: /TestBomba/
+        public ActionResult Index()
+        {
+            CARD.Test();
+            return View();
+        }
+
+    }
+}
