ee5bcdc119cbbd1343ef94dd58b49c13fa575ff5
Baltazar López <baltazarh_lopez@hotmail.com>
2015-11-15 10:56:07 -0600

    Ya muestra producto de proveedor para compra

--- ISW_GASISW/Controllers/OrdenCompraController.cs
+++ ISW_GASISW/Controllers/OrdenCompraController.cs
@@ -15,17 +15,42 @@ namespace ISW_GASISW.Controllers
 
         //
         // GET: /OrdenCompra/
-
         public ActionResult Index()
-        {           
+        {
+            Session["M_O_C"] = null;
+            ViewBag.PROVEEDOR_id = new SelectList(db.proveedor, "id", "nombre");
             return View();
         }
 
+        //
+        // POST: /OrdenCompra/
+        [HttpPost]
+        public ActionResult Index(M_M_Orden_Compra MMOC)
+        {
+            m_orden_compra MOP = new m_orden_compra();
+            MOP.fecha_emitida = DateTime.Today;
+            MOP.emitido_EMPLEADO_id = Convert.ToInt16(Session["Empleado_id"]);
+            MOP.estado = false;
+            MOP.PROVEEDOR_id = MMOC.proveedor;
+
+            db.m_orden_compra.Add(MOP);
+            db.SaveChanges();
+
+            int MASTER = Convert.ToInt16(db.m_orden_compra.Max(x => x.id));
+            Session["M_O_C"] = MASTER;
+
+            return RedirectToAction("Create");
+        }
+
+        //
+        // GET: /OrdenCompra/Create
         public ActionResult Create()
         {
-            ViewBag.PROVEEDOR_id = new SelectList(db.proveedor, "id", "nombre");
-            var lstProducto = new List<producto>(db.producto);
-            return View(lstProducto);
+            int Maestro = Convert.ToInt16(Session["M_O_C"]);
+            int proveedor = Convert.ToInt16(db.m_orden_compra.Where(x => x.id == Maestro).Select(X => X.PROVEEDOR_id).Single());
+            List<producto> Producto = db.producto.Include(p => p.categoria_producto).Include(p => p.presentacion_producto).Where(x => x.PROVEEDOR_id == proveedor).ToList();
+            ViewBag.PRODUCTO = Producto;
+            return View();
         }
 
     }
---
+++ ISW_GASISW/Models/M_M_Orden_Compra.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISW_GASISW.Models
+{
+    public class M_M_Orden_Compra
+    {
+        public int proveedor { get; set; }
+    }
+}
