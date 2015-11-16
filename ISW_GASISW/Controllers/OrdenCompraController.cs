d1dd66416806e448edbe17b8a7ef6f8a81ed41d8
Baltazar López <baltazarh_lopez@hotmail.com>
2015-11-15 23:36:45 -0600

    Ya muestra los valores correctos

--- ISW_GASISW/Controllers/OrdenCompraController.cs
+++ ISW_GASISW/Controllers/OrdenCompraController.cs
@@ -19,7 +19,12 @@ namespace ISW_GASISW.Controllers
         {
             Session["M_O_C"] = null;
             ViewBag.PROVEEDOR_id = new SelectList(db.proveedor, "id", "nombre");
-            return View();
+            //Provando 
+            var Lista = db.d_orden_compra.Select(p => p.m_orden_compra.id).Take(1);
+            List<m_orden_compra> Lista2 = db.m_orden_compra.Include(p => p.d_orden_compra).Where(p => Lista.Contains(p.id)).ToList();
+            M_M_Orden_Compra MMOC = new M_M_Orden_Compra();
+            MMOC.LMOC = Lista2;            
+            return View(MMOC);
         }
 
         //
--- ISW_GASISW/Models/M_M_Orden_Compra.cs
+++ ISW_GASISW/Models/M_M_Orden_Compra.cs
@@ -8,5 +8,7 @@ namespace ISW_GASISW.Models
     public class M_M_Orden_Compra
     {
         public int proveedor { get; set; }
+        public List<m_orden_compra> LMOC { get; set; }
     }
+
 }
