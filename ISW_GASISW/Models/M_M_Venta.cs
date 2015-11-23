dd3627d66ca39efe873545724815119462eef4f6
Baltazar López <baltazarh_lopez@hotmail.com>
2015-11-22 20:07:39 -0600

    Ya lista las Ventas

---
+++ ISW_GASISW/Models/M_M_Venta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISW_GASISW.Models
+{
+    public class M_M_Venta
+    {
+        public List<m_venta> ListaMV { get; set; }
+    }
+}
