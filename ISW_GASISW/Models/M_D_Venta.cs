0ceda31d1371cdff8f30da12089042a6e244eceb
Baltazar López <baltazarh_lopez@hotmail.com>
2015-11-22 18:49:12 -0600

    ya guarda venta

---
+++ ISW_GASISW/Models/M_D_Venta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISW_GASISW.Models
+{
+    public class M_D_Venta
+    {
+        public int TipoFacturacion { get; set; }
+        public string[] id { get; set; }
+        public string[] Cant { get; set; }
+    }
+}
